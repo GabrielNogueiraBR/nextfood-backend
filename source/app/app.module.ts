9b4e7f23f74e2cd5a930d248cbe07b1c91deeae0
Vitor Gois <vitorcgois777@gmail.com>
2022-04-24 01:46:10 -0300

    chore: restructure domain folders

--- source/app/app.module.ts
+++ source/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import configuration from '../config/env-vars';
+import { RestaurantModule } from '../restaurant/restaurant.module';
+import { DatabaseModule } from './../database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import configuration from './config/configuration';
 
 @Module({
   imports: [
@@ -13,6 +15,8 @@ import configuration from './config/configuration';
         configuration,
       ],
     }),
+    DatabaseModule,
+    RestaurantModule,
   ],
   controllers: [
     AppController,
--- source/config/env-vars.ts
+++ source/config/env-vars.ts
@@ -3,4 +3,8 @@ export default () => ({
   app: {
     port: Number.parseInt(process.env.PORT, 10) || 8080,
   },
+  database: {
+    secret: String(process.env.FAUNADB_SECRET),
+    region: String(process.env.FAUNADB_REGION),
+  },
 });
