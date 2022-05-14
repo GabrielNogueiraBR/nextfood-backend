999c3d6d61444300259980da054ba34ceaaf4b25
Vitor Gois <vitorcgois777@gmail.com>
2022-05-14 18:20:29 -0300

    feat: relate franchise entity to franchise_schedule

--- source/franchise/franchise.entity.ts
+++ source/franchise/franchise.entity.ts
@@ -1,8 +1,9 @@
 import { IsBoolean, IsDate, IsObject, IsString, IsUUID } from 'class-validator';
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 import { Address } from '../address/address.entity';
 import { Restaurant } from '../restaurant/restaurant.entity';
+import { WeekDay } from '../utils/weekday.enum';
 
 @Entity()
 export class Franchise {
@@ -32,6 +33,48 @@ export class Franchise {
   @JoinColumn()
   @IsObject()
   public address!: Address;
+
+  @OneToMany(() => FranchiseSchedule, (schedule) => schedule.franchise, {
+    onDelete: 'CASCADE', nullable: false,
+  })
+  @IsObject()
+  public schedule!: FranchiseSchedule[];
+
+  @CreateDateColumn({ type: 'timestamp' })
+  @IsDate()
+  public createdAt!: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  @IsDate()
+  public updatedAt!: Date;
+
+}
+
+@Entity()
+export class FranchiseSchedule {
+
+  @PrimaryGeneratedColumn('uuid')
+  @IsUUID()
+  public id: string;
+
+  @Column({ type: 'enum', enum: WeekDay })
+  @IsString()
+  public weekDay: WeekDay;
+
+  @Column({ type: 'timestamp' })
+  @IsString()
+  public start_datetime: Date;
+
+  @Column({ type: 'timestamp' })
+  @IsString()
+  public end_datetime: Date;
+
+  @OneToOne(() => Franchise, (franchise) => franchise.address, {
+    nullable: false,
+  })
+  @JoinColumn()
+  @IsObject()
+  public franchise: Franchise;
 
   @CreateDateColumn({ type: 'timestamp' })
   @IsDate()
